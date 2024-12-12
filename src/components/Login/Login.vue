5da4b33903d8bed803dc0ea646b08341757f770c
ven4k <barryven@yandex.ru>
2024-12-12 21:55:19 +0300

    final

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -31,7 +31,6 @@ const password = ref("");
 const isWrongData = ref(false);
 
 onMounted(() => {
-  sessionStorage.clear()
   sessionStorage.setItem("login", "admin");
   sessionStorage.setItem("login2", "manager");
   sessionStorage.setItem("password", "admin");
